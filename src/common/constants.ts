14bc80a53488dbd39c28ba4d9a08036430f8c288
Artsiom Murashko <ksarelto@tut.by>
2022-07-10 21:23:58 +0300

    [develop] Common files

---
+++ src/common/constants.ts
@@ -0,0 +1,10 @@
+export const errMess = {
+  invalid: 'Invalid id of ',
+  notExist: ' is not exist',
+  invalidPass: 'Invalid user password',
+  errorToken: 'JWT token secret was not set',
+  emptyToken: 'There is no token in request headers!',
+  invalidToken: 'This token is invalid',
+  existUser: 'User with such login has already been exist',
+  errDownload: 'Error during downloading',
+};
